19a01aa6a49921b218fcaba67ff14437425d3d65
1191510903 <1191510903@qq.com>
2020-11-05 20:45:47 +0800

    eureka

--- cloud-consumer-order8080/src/main/java/com/zhang/springcloud/controller/OrderController.java
+++ cloud-consumer-order8080/src/main/java/com/zhang/springcloud/controller/OrderController.java
@@ -15,12 +15,13 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-    public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
     @Resource
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
+
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
